48379104a0bb4080256b356ca802d877882c1b51
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-01-12 21:33:07 +0000

    arrya section

---
+++ dayone/dayoneCwithArrays.js
@@ -0,0 +1,14 @@
+const a = [];
+a.push(1);
+a.pop(0);
+a.push(1,2,3,4);
+ 
+//console.log(a);
+
+const arr = [1, '123',[23,09,90]];
+ console.log(arr[2][2]);
+//console.log(arr);
+
+
+const b = Array.of(23,43,34,(55,34));
+//console.log(b);
