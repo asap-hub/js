6d3ad4f4e3564f622b27f2c751ba86a64d7b9a24
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-01-13 10:54:43 +0000

    dayone done

--- dayone/dayoneloop.js
+++ dayone/dayoneloop.js
@@ -1,15 +1,26 @@
 const item = [2,30,20,10]
 let x = 0;
 while(x < item.length){
-    console.log(`item index: ${item[x]}`);
-    console.log(x);
+    //console.log(`item index: ${item[x]}`);
+    //console.log(x);
     x = x+1;
 }
 var y;
 for(y of item)
 {
-    console.log(`we looping wosh!!!: ${y}`);
+  //  console.log(`we looping wosh!!!: ${y}`);
     
    
 }
-console.log(item.join(y));
+//console.log(item.join(y));
+
+const x_axis = [1,2,3,4,5];
+const y_axis = [1,2,3,4,5];
+
+for(let m = 0; m < x_axis.lenght; m++)
+  { 
+    
+    console.log(`x-axis: ${x_axis[m]}`);
+    console.log(`x-axis: ${m}`);
+    
+  }
