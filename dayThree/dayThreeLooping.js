6582a4e51e7f51b6767db14df714dd58e079719c
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-01-16 01:05:22 +0000

    day 3 done

--- dayThree/dayThreeLooping.js
+++ dayThree/dayThreeLooping.js
@@ -1,29 +1,32 @@
-const arr = ["Western North","Western Region", "Ashanti region", "Greater Accra", 203];
+const arr = [
+  "Western North",
+  "Western Region",
+  "Ashanti region",
+  "Greater Accra",
+  203,
+];
 
-for(let i = 0; i < arr.length; i++){
-    console.log(arr[i]);
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
 var result = arr.forEach((s) => {
-    console.log(s);
-}); 
+  console.log(s);
+});
 
-for(let ar of arr){
-    if(typeof ar !== "string") continue
+for (let ar of arr) {
+  if (typeof ar !== "string") continue;
 
-    console.log(ar);
+  console.log(ar);
 }
 
 let x = 0;
 
-do{
-    console.log(arr);
-    x++;
-}
-while(x < arr.length);
+do {
+  console.log(arr);
+  x++;
+} while (x < arr.length);
 
-while(x < arr.length){
-console.log(arr[x]);
-x++;
-
+while (x < arr.length) {
+  console.log(arr[x]);
+  x++;
 }
- 
--- dayone/dayoneloop.js
+++ dayone/dayoneloop.js
@@ -1,26 +1,20 @@
-const item = [2,30,20,10]
+const item = [2, 30, 20, 10];
 let x = 0;
-while(x < item.length){
-    //console.log(`item index: ${item[x]}`);
-    //console.log(x);
-    x = x+1;
+while (x < item.length) {
+  //console.log(`item index: ${item[x]}`);
+  //console.log(x);
+  x = x + 1;
 }
 var y;
-for(y of item)
-{
+for (y of item) {
   //  console.log(`we looping wosh!!!: ${y}`);
-    
-   
 }
 //console.log(item.join(y));
 
-const x_axis = [1,2,3,4,5];
-const y_axis = [1,2,3,4,5];
+const x_axis = [1, 2, 3, 4, 5];
+const y_axis = [1, 2, 3, 4, 5];
 
-for(let m = 0; m < x_axis.lenght; m++)
-  { 
-    
-    console.log(`x-axis: ${x_axis[m]}`);
-    console.log(`x-axis: ${m}`);
-    
-  }
+for (let m = 0; m < x_axis.lenght; m++) {
+  console.log(`x-axis: ${x_axis[m]}`);
+  console.log(`x-axis: ${m}`);
+}
