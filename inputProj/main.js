599c026bb1371ddb6eebcc2077d50b87132ac48a
Abdallah Mustapha <abdullahmustapha59@gmail.com>
2023-01-17 17:10:48 +0000

    style added

--- inputProj/main.js
+++ inputProj/main.js
@@ -1,2 +1,37 @@
-let submitMe = document.getElementsByClassName(".button-me")
-console.log("im click",submitMe);
+let submitMe = document.querySelector(".button-me"); 
+
+
+//get data
+// let getfirstName = document.getElementById("#fn");
+// let getsecondName = document.getElementById("#ln");
+// let getage = document.getElementById("#ag");
+ 
+
+ 
+    function sendData() {
+
+        var firstName = document.getElementById('fName').value;
+        var secondName = document.getElementById('lName').value;
+        var age = document.getElementById('age').value;
+
+        const car = {
+            firstName :  firstName,
+            secondName: secondName,
+            age: age
+        }
+ 
+        
+        var vehicle = [car];
+    
+        window.sessionStorage.setItem('vehicle', JSON.stringify(vehicle));
+    
+      
+    }
+  
+   let btn = submitMe.addEventListener('click', () =>(sendData()));
+ 
+    // window.onload = function(){
+    //     document.getElementById('carForm').onsubmit = sendData;
+        
+    // }
+ 
